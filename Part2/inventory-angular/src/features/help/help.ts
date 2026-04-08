51db0c7436a3bd46248f79415814ffb04e9b5912
赵子杭 <yc3072513656@126.com>
2026-04-08 19:13:39 +0800

    - Basically completed the help page help component

---
+++ Part2/inventory-angular/src/features/help/help.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  standalone: true,
+  imports: [],
+  templateUrl: './help.html',
+  styleUrls: ['./help.css']
+})
+export class HelpComponent {
+  title = 'Help & FAQs';
+}
