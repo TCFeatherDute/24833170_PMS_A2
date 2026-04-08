e8688315678d26fe2cf929a12acf75a218be89fe
赵子杭 <yc3072513656@126.com>
2026-04-08 19:12:32 +0800

    - Complete the privacy security privacy component

---
+++ Part2/inventory-angular/src/features/privacy/privacy.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy.html',
+  styleUrls: ['./privacy.css']
+})
+export class PrivacyComponent {
+  title = 'Privacy & Security Analysis';
+}
